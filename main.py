e01fd360025f3ecb749eb60dd859d4f29bd6577f
Marron Akanishi <marron.general@gmail.com>
2018-01-21 01:53:28 +0900

    miss

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 import time
 import tweepy as tp
@@ -14,22 +15,18 @@ with open("birth.csv","r",encoding="utf-8") as csv:
 
 auth = oauth.get_oauth()
 api = tp.API(auth)
-is_birth = False
-old_date = datetime.date.today().strftime("%m/%d")
+date = datetime.date.today().strftime("%m/%d")
 
-# 監視
-while(1):
-    if old_date != datetime.date.today().strftime("%m/%d"):
-        old_date = datetime.date.today().strftime("%m/%d")
-        try:
-            name = birthlist[old_date]
-            icon = iconlist[old_date]
-            if not is_birth:
-                urllib.request.urlretrieve(api.me().profile_image_url.replace("normal","400x400"),"./icon.jpg")
-            api.update_profile_image(icon)
-            api.update_status("本日は{}の誕生日です".format(name))
-            is_birth = True
-        except KeyError:
-            api.update_profile_image("./icon.jpg")
-            is_birth = False
-    time.sleep(60)
+try:
+    name = birthlist[date]
+    icon = iconlist[date]
+    if not os.path.exists("./icon.jpg"):
+        urllib.request.urlretrieve(api.me().profile_image_url.replace("normal","400x400"),"./icon.jpg")
+    api.update_profile_image(icon)
+    api.update_status("本日は{}の誕生日です".format(name))
+except:
+    if os.path.exists("./icon.jpg"):
+        api.update_profile_image("./icon.jpg")
+        os.remove("./icon.jpg")
+
+#api.update_status("日付確認完了")
